cd13fc89b9008ab96bd53cc23ec4bdfa307cd81e
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-06-27 09:04:52 +0200

    Enhance Samples class with detailed documentation
    - Added XML comments to clarify the purpose of TicketViewModel instances.
    - Documented the creation of an array of TicketViewModel for better understanding.

--- BlazorCookbookApp.Client/Chapters/Chapter1/Data/Samples.cs
+++ BlazorCookbookApp.Client/Chapters/Chapter1/Data/Samples.cs
@@ -3,11 +3,18 @@ namespace BlazorCookbook.App.Client.Chapters.Chapter01.Data;
 
 public static class Samples
 {
+    /// <summary>
+    /// We create 3 single instances of TicketViewModel.
+    /// TicketViewModel is a record defined in Data/TicketViewModel.cs.
+    /// </summary>
     public static readonly TicketViewModel
         Adult = new("Adult", 10.00m, 100),
         FreeAdmission = new("Free Admission", 0.00m, 100),
         Elderly = new("Elderly", 8.00m, 10);
 
+    /// <summary>
+    /// We create an array of TicketViewModel instances.
+    /// </summary>
     public static readonly TicketViewModel[] Tickets = [
         new("Adult", 10.00m, 100),
         new("Free Admission", 0.00m, 100),
